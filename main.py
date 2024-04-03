32a870dd8e4b2c2d96e52f9f8354cb686aab80de
gauyex-bzz <gauyex@bzz.ch>
2024-04-03 09:38:51 +0200

    quicksort should be sorting by filename but somehow it isn't fully working

--- main.py
+++ main.py
@@ -3,32 +3,42 @@ import csv
 
 
 def list_files(startpath):
-    # Create a list to store file information
     file_list = []
     for root, dirs, files in os.walk(startpath):
         for filename in files:
-            # Join the root path with the file name to get the full file path
             filepath = os.path.join(root, filename)
-            # Get the size of the file in bytes
             size = os.path.getsize(filepath)
-            # Append file information to the list
-            file_list.append((filename, size, filepath))
+            file_list.append((filepath, size, filename))
     return file_list
 
 
-# Path to C drive
-c_drive_path = 'C:\\Program Files\Rockstar Games'
+def quicksort(file_list):
+    for i in range(len(file_list)):
+        pivot = (file_list[i])
+        left = []
+        right = []
+        equal = []
 
-# Get list of files in C drive
-files = list_files(c_drive_path)
+        for filename in file_list:
+            if filename < pivot:
+                left.append(filename)
+            elif filename > pivot:
+                right.append(filename)
+            elif filename == pivot:
+                equal.append(filename)
+            file_list = left + equal + right
+        return file_list
 
-# Write file information to a CSV file
-with open('c.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter=';')  # Set the delimiter to semicolon
-    # Write header
-    writer.writerow(['File Name', 'Size (bytes)', 'File Path'])
-    # Write file information
-    for file_info in files:
-        writer.writerow(file_info)
 
-print("File Created")
+def main():
+    c_drive_path = 'C:\\Program Files\Rockstar Games'
+
+    files = list_files(c_drive_path)
+
+    with open('c.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=';')
+        writer.writerow(['File Name', 'Size (bytes)', 'File Path'])
+        for file_info in files:
+            writer.writerow(file_info)
+
+    print("File Created")
